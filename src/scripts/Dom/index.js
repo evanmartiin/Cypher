4273015209d94a10fdfc6dc83d3964f9f9b9d340
justinqllvr <justin.quillevere@outlook.fr>
2023-06-20 16:21:49 +0200

    UI Wip

--- src/scripts/Dom/index.js
+++ src/scripts/Dom/index.js
@@ -1,10 +1,13 @@
 import { Cursor } from './Cursor.js';
+import { Ui } from './Ui.js';
 
 function createDomModules() {
 	const cursor = new Cursor();
+	const ui = new Ui();
 
 	return {
 		cursor,
+		ui
 	};
 }
 
