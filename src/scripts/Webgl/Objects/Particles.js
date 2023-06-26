9a00559313c69f5b4e41cd69741a9206fce66689
Thoma Lecornu <71992086+ThomaLecornu@users.noreply.github.com>
2023-06-26 11:45:21 +0200

    button design

--- src/scripts/Webgl/Objects/Particles.js
+++ src/scripts/Webgl/Objects/Particles.js
@@ -61,9 +61,9 @@ export class Particles extends Group {
 	}
 
 	_createGeometry() {
-		// const baseGeometry = new PlaneGeometry(1, 1, 1, 1);
+		const baseGeometry = new PlaneGeometry(1, 1, 1, 1);
 		// const baseGeometry = new BoxGeometry(1, 1, 1, 1);
-		const baseGeometry = new OctahedronGeometry(1, 0);
+		// const baseGeometry = new OctahedronGeometry(1, 0);
 		// const baseGeometry = new SphereGeometry();
 		// const baseGeometry = app.core.assetsManager.get('cube').children[0].geometry;
 		baseGeometry.scale(2, 2, 2);
